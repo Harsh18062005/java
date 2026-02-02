332e76ed40c589df3188b695084e6732c30408f5
Harsh Juneja <junejaharsh1806@gmail.com>
2026-02-02 12:17:17 +0530

    Literals in Java

---
+++ Literal.java
@@ -0,0 +1,17 @@
+class Literal{
+    public static void main(String args[]){
+        int a=0xb1111; 
+        float b=0123.222f;
+        double  c=0x1.8p3;
+        char  c='A';
+        String d="Hello"; 
+        boolean e=true; 
+
+        System.out.println("Integer Literal: " + a);
+        System.out.println("Float Literal: " + b);
+       System.out.println("Double Literal: " + c);
+        System.out.println("String Literal: " + d);
+        System.out.println("Boolean Literal: " + e);
+    }
+
+}
