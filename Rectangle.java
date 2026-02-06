1e742c4ab6bb1e55d938592879659d012dff5127
Harsh Juneja <junejaharsh1806@gmail.com>
2026-02-06 09:18:00 +0530

    Area of Rectangle using this keyword in java

---
+++ Rectangle.java
@@ -0,0 +1,38 @@
+
+class Rectangle{
+    int length, width;
+    /*void area(int x,int y){
+        length=x;
+        width=y;
+        int area=length*width;
+        System.out.println("Length of rectangle is: "+length);
+        System.out.println("Width of rectangle is: "+width);
+        System.out.println("Area of rectangle is: "+area);
+    }
+    public static void main(String args[]){
+        Rectangle rect=new Rectangle();//object  of class  creation
+        rect.area(5,10);
+    }
+}
+*/
+
+        //Implementation of this Keyword for area of rectangle
+class Rectangle {       
+    int length;
+    int width;
+
+    Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    void area() {
+        int result = this.length * this.width;
+        System.out.println("Area of rectangle: " + result);
+    }
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(10, 5);
+        r.area();
+    }
+}
